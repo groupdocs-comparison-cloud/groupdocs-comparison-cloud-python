f970b22fae7a791d07b756c2d418217fd368c289
product-team <product.team@groupdocs.com>
2021-07-16 01:44:18 -0500

    Updated sources

--- groupdocs_comparison_cloud/configuration.py
+++ groupdocs_comparison_cloud/configuration.py
@@ -202,6 +202,6 @@ class Configuration(object):
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 21.3\n"\
-               "SDK Package Version: 21.3.1".\
+               "Version of the API: 21.7\n"\
+               "SDK Package Version: 21.7".\
                format(env=sys.platform, pyversion=sys.version)
